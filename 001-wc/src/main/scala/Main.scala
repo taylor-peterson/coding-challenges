c714be010a57973f54a01b0a3140a844a4d0ab91
Taylor Peterson <tpeterson@metropolis.io>
2024-05-05 17:32:07 -0700

    Add minimal example of scopt integration

--- 001-wc/src/main/scala/Main.scala
+++ 001-wc/src/main/scala/Main.scala
@@ -1,3 +1,27 @@
+import scopt.OParser
+
+case class Config(
+    foo: Int = -1,
+    xyz: Boolean = false,
+    maxCount: Int = -1,
+)
+
 object Main extends App {
-  println("Hello, Taylor!")
+  val builder = OParser.builder[Config]
+
+  val parser1 = {
+    import builder._
+    OParser.sequence(
+        programName("scopt"),
+        head("scopt", "4.x"),
+        opt[Int]('f', "foo")
+      .action((x, c) => c.copy(foo = x))
+      .text("foo is an integer property"),
+    help("help").text("prints this usage text"),
+    )
+  }
+  OParser.parse(parser1, args, Config()) match {
+    case Some(config) => println(OParser.usage(parser1))
+    case _ =>
+  }
 }
