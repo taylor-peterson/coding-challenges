28d5bab9297f320c849fcecdb80f36dee3bb6092
Taylor Peterson <tpeterson@metropolis.io>
2024-05-11 05:59:53 -0700

    Add remaining options to scopt

--- 001-wc/src/main/scala/Main.scala
+++ 001-wc/src/main/scala/Main.scala
@@ -1,27 +1,48 @@
 import scopt.OParser
 
 case class Config(
-    foo: Int = -1,
-    xyz: Boolean = false,
-    maxCount: Int = -1,
+    bytes: Boolean = false,
+    lines: Boolean = false,
+    words: Boolean = false,
+    chars: Boolean = false,
+    file: String = "-",
 )
 
 object Main extends App {
-  val builder = OParser.builder[Config]
+  private val builder = OParser.builder[Config]
 
-  val parser1 = {
+  private val parser1 = {
     import builder._
     OParser.sequence(
-        programName("scopt"),
-        head("scopt", "4.x"),
-        opt[Int]('f', "foo")
-      .action((x, c) => c.copy(foo = x))
-      .text("foo is an integer property"),
-    help("help").text("prints this usage text"),
+      programName("ccwc"),
+      head("ccwc", "0.1"),
+      opt[Unit]('c', "bytes")
+        .action((_, c) => c.copy(bytes = true))
+        .text("print the byte counts"),
+      opt[Unit]('l', "lines")
+        .action((_, c) => c.copy(lines = true))
+        .text("print the line counts"),
+      opt[Unit]('w', "words")
+        .action((_, c) => c.copy(words = true))
+        .text("print the word counts"),
+      opt[Unit]('m', "chars")
+        .action((_, c) => c.copy(chars = true))
+        .text("print the char counts"),
+      opt[String]("file")
+        .action((file, c) => c.copy(file = file))
+        .text("file to process (defaults to '-' meaning STDIN)"),
+      help("help")
+        .abbr("h")
+        .text("prints this usage text"),
     )
   }
+
+  def run(config: Config): Unit = {
+    println(config)
+  }
+
   OParser.parse(parser1, args, Config()) match {
-    case Some(config) => println(OParser.usage(parser1))
-    case _ =>
+    case Some(config) => run(config)
+    case _            => // Messaging handled by OParser
   }
 }
