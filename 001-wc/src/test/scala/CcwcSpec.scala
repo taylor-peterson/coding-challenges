f32e303a4feb8a04e9965e8b222089b11bf157c4
Taylor Peterson <tpeterson@hmc.edu>
2024-05-28 05:03:04 -0700

    Test fixture added; tests refactored to use

--- 001-wc/src/test/scala/CcwcSpec.scala
+++ 001-wc/src/test/scala/CcwcSpec.scala
@@ -1,17 +1,22 @@
-import org.scalatest.fixture
+import org.scalatest.Outcome
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.FixtureAnyWordSpec
 
 import scala.sys.process._
 
-class CcwcSpec extends FixtureAnyWordSpec with fixture.ConfigMapFixture with Matchers {
+class CcwcSpec extends FixtureAnyWordSpec with Matchers {
+  val testTxtPath: String = getClass.getResource("test.txt").getPath
+
+  type FixtureParam = String
+  def withFixture(test: OneArgTest): Outcome = {
+    val targetDir = test.configMap.getRequired[String]("targetDir")
+    val ccwc: FixtureParam = targetDir + s"/native-image/ccwc"
+    withFixture(test.toNoArgTest(ccwc))
+  }
+
   "ccwc" when {
     "-h" should {
-      "display usage information" in { configMap =>
-        val targetDir = configMap.getRequired[String]("targetDir")
-        val ccwc = targetDir + s"/native-image/ccwc"
-        val command = ccwc + s" -h"
-        val output = command.!!
+      "display usage information" in { ccwc =>
         val expectedUsage =
           """ccwc 0.1
             |Usage: ccwc [options] [file]
@@ -23,66 +28,37 @@ class CcwcSpec extends FixtureAnyWordSpec with fixture.ConfigMapFixture with Mat
             |  file         file to process (defaults to '-' meaning STDIN)
             |  -h, --help   prints this usage text
             |""".stripMargin
-        output shouldBe expectedUsage
+        (ccwc + " -h").!! shouldBe expectedUsage
       }
     }
     "-c with test file" should {
-      "return the correct byte count" in { configMap =>
-        val testTxtPath = getClass.getResource("test.txt").getPath
-        val targetDir = configMap.getRequired[String]("targetDir")
-        val ccwc = targetDir + s"/native-image/ccwc"
-        val command = ccwc + s" -c ${testTxtPath}"
-        val output = command.!!
-        output shouldBe s"342190\t${testTxtPath}\n"
+      "return the correct byte count" in { ccwc =>
+        (ccwc + s" -c $testTxtPath").!! shouldBe s"342190\t$testTxtPath\n"
       }
     }
     "-l with test file" should {
-      "return the correct line count" in { configMap =>
-        val testTxtPath = getClass.getResource("test.txt").getPath
-        val targetDir = configMap.getRequired[String]("targetDir")
-        val ccwc = targetDir + s"/native-image/ccwc"
-        val command = ccwc + s" -l ${testTxtPath}"
-        val output = command.!!
-        output shouldBe s"7145\t${testTxtPath}\n"
+      "return the correct line count" in { ccwc =>
+        (ccwc + s" -l $testTxtPath").!! shouldBe s"7145\t$testTxtPath\n"
       }
     }
     "-w with test file" should {
-      "return the correct word count" in { configMap =>
-        val testTxtPath = getClass.getResource("test.txt").getPath
-        val targetDir = configMap.getRequired[String]("targetDir")
-        val ccwc = targetDir + s"/native-image/ccwc"
-        val command = ccwc + s" -w ${testTxtPath}"
-        val output = command.!!
-        output shouldBe s"58164\t${testTxtPath}\n"
+      "return the correct word count" in { ccwc =>
+        (ccwc + s" -w $testTxtPath").!! shouldBe s"58164\t$testTxtPath\n"
       }
     }
     "-m with test file" should {
-      "return the correct character count" in { configMap =>
-        val testTxtPath = getClass.getResource("test.txt").getPath
-        val targetDir = configMap.getRequired[String]("targetDir")
-        val ccwc = targetDir + s"/native-image/ccwc"
-        val command = ccwc + s" -m ${testTxtPath}"
-        val output = command.!!
-        output shouldBe s"339292\t${testTxtPath}\n"
+      "return the correct character count" in { ccwc =>
+        (ccwc + s" -m $testTxtPath").!! shouldBe s"339292\t$testTxtPath\n"
       }
     }
     "with test file" should {
-      "return the correct counts for bytes, lines, and words" in { configMap =>
-        val testTxtPath = getClass.getResource("test.txt").getPath
-        val targetDir = configMap.getRequired[String]("targetDir")
-        val ccwc = targetDir + s"/native-image/ccwc"
-        val command = ccwc + s" ${testTxtPath}"
-        val output = command.!!
-        output shouldBe s"7145\t58164\t342190\t${testTxtPath}\n"
+      "return the correct counts for bytes, lines, and words" in { ccwc =>
+        (ccwc + s" $testTxtPath").!! shouldBe s"7145\t58164\t342190\t$testTxtPath\n"
       }
     }
     "with stdin" should {
-      "return the correct counts for bytes, lines, and words" in { configMap =>
-        val testTxtPath = getClass.getResource("test.txt").getPath
-        val targetDir = configMap.getRequired[String]("targetDir")
-        val ccwc = targetDir + s"/native-image/ccwc"
-
-        (s"cat ${testTxtPath}" #| ccwc).!! shouldBe s"7145\t58164\t342190\t\n"
+      "return the correct counts for bytes, lines, and words" in { ccwc =>
+        (s"cat $testTxtPath" #| ccwc.toString()).!! shouldBe s"7145\t58164\t342190\t\n"
       }
     }
   }
