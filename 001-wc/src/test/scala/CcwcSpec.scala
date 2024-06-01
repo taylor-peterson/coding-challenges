e1fa1789e7e6dd2fa83445d459d823834cb7f758
Taylor Peterson <tpeterson@hmc.edu>
2024-06-01 06:14:22 -0700

    Parameterize tests

--- 001-wc/src/test/scala/CcwcSpec.scala
+++ 001-wc/src/test/scala/CcwcSpec.scala
@@ -1,10 +1,12 @@
 import org.scalatest.Outcome
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.prop.Tables.Table
 import org.scalatest.wordspec.FixtureAnyWordSpec
 
 import scala.sys.process._
 
-class CcwcSpec extends FixtureAnyWordSpec with Matchers {
+class CcwcSpec extends FixtureAnyWordSpec with Matchers with TableDrivenPropertyChecks {
   val testTxtPath: String = getClass.getResource("test.txt").getPath
 
   type FixtureParam = String
@@ -13,6 +15,8 @@ class CcwcSpec extends FixtureAnyWordSpec with Matchers {
     val ccwc: FixtureParam = targetDir + s"/native-image/ccwc"
     withFixture(test.toNoArgTest(ccwc))
   }
+
+  def returnCounts = afterWord("return the correct counts for")
 
   "ccwc" when {
     "-h" should {
@@ -33,29 +37,20 @@ class CcwcSpec extends FixtureAnyWordSpec with Matchers {
         (ccwc + " -h").!! shouldBe expectedUsage
       }
     }
-    "-c with test file" should {
-      "return the correct byte count" in { ccwc =>
-        (ccwc + s" -c $testTxtPath").!! shouldBe s"342190\t$testTxtPath\n"
-      }
-    }
-    "-l with test file" should {
-      "return the correct line count" in { ccwc =>
-        (ccwc + s" -l $testTxtPath").!! shouldBe s"7145\t$testTxtPath\n"
-      }
-    }
-    "-w with test file" should {
-      "return the correct word count" in { ccwc =>
-        (ccwc + s" -w $testTxtPath").!! shouldBe s"58164\t$testTxtPath\n"
-      }
-    }
-    "-m with test file" should {
-      "return the correct character count" in { ccwc =>
-        (ccwc + s" -m $testTxtPath").!! shouldBe s"339292\t$testTxtPath\n"
-      }
-    }
-    "with test file" should {
-      "return the correct counts for bytes, lines, and words" in { ccwc =>
-        (ccwc + s" $testTxtPath").!! shouldBe s"7145\t58164\t342190\t$testTxtPath\n"
+    "counting from test file" should returnCounts {
+      val countsTable = Table(
+        ("test case", "args", "counts"),
+        ("bytes", "-c", 342190),
+        ("lines", "-l", 7145),
+        ("words", "-w", 58164),
+        ("characters", "-m", 339292),
+        ("no args", "", "7145\t58164\t342190"),
+      )
+
+      forEvery(countsTable) { (testCase, args, counts) =>
+        testCase in { ccwc =>
+          (ccwc + s" $args $testTxtPath").!! shouldBe s"$counts\t$testTxtPath\n"
+        }
       }
     }
     "with stdin" should {
